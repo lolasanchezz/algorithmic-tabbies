c8add383520ae0d61ec5c2063212fce0cfe8d765
lolasanchezz <lolansanchez@icloud.com>
2024-08-13 14:11:11 -0400

    random cat face generation works now

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const width = 125;
 const height = 125;
 const padding = 2;
+const allLines = [];
 setDocDimensions(width, height);
 
 const availableHeight = height - padding;
@@ -12,19 +13,33 @@ const tallnessOfFace = bt.randInRange((availableHeight*0.3), (availableHeight*0.
 const widthOfEar = bt.randInRange(availableWidth * 1/3, availableWidth * 1/6);
 const widthOfHalfEar = widthOfEar*(bt.randInRange(1/4, 1/2));
   
-const middleLeftAnchor = [padding, tallnessOfFace];
-const bottomAnchor = [padding, padding];
+const leftTopAnchor = [padding, tallnessOfFace];
+const leftBottomAnchor = [padding, padding];
 
 const topLeftEarAnchor = [widthOfHalfEar, availableWidth];
 const leftMiddleAnchor = [widthOfEar, tallnessOfFace];
 
-const rightMiddleAnchor = [(availableWidth-widthOfEar), tallnessOfFace)];
-const topRightEarAnchor = [(availableWidth-widthOfHalfEar), 
+const rightMiddleAnchor = [(availableWidth-widthOfEar), tallnessOfFace];
+const topRightEarAnchor = [(availableWidth-widthOfHalfEar), availableWidth];
 
+const rightTopAnchor = [(availableWidth-padding), tallnessOfFace];
+const rightBottomAnchor = [(availableWidth-padding), padding];
 
+const looseCatOutline = [
+  leftTopAnchor,
+  topLeftEarAnchor,
+  leftMiddleAnchor,
+  rightMiddleAnchor,
+  topRightEarAnchor,
+  rightTopAnchor,
+  rightBottomAnchor,
+  leftBottomAnchor,
+  leftTopAnchor
+  ];
 
+allLines.push(looseCatOutline);
 
-
+drawLines(allLines);
 
 
 
