3f2c4768646dec0c090856d8ab93cd3b50305426
lolasanchezz <lolansanchez@icloud.com>
2024-08-13 14:48:57 -0400

    eyes bounding box done but left x coords not right

--- main.js
+++ main.js
@@ -2,28 +2,31 @@ const width = 125;
 const height = 125;
 const padding = 2;
 const allLines = [];
+const r = bt.randInRange;
 setDocDimensions(width, height);
 
 const availableHeight = height - padding;
 const availableWidth = width - padding;
 //drawing the cat outline
-
-//RULES FOR THE ANCHORS: left three points add up to the height - padding
 const tallnessOfFace = bt.randInRange((availableHeight*0.3), (availableHeight*0.5));
-const widthOfEar = bt.randInRange(availableWidth * 1/3, availableWidth * 1/6);
+const widthOfFace = availableWidth * (r(0.6, 1));
+
+const widthOfEar = bt.randInRange(widthOfFace * 1/3, widthOfFace * 1/6);
 const widthOfHalfEar = widthOfEar*(bt.randInRange(1/4, 1/2));
-  
-const leftTopAnchor = [padding, tallnessOfFace];
-const leftBottomAnchor = [padding, padding];
+const leftBound = ((availableWidth-widthOfFace)/2)
 
-const topLeftEarAnchor = [widthOfHalfEar, availableWidth];
-const leftMiddleAnchor = [widthOfEar, tallnessOfFace];
 
-const rightMiddleAnchor = [(availableWidth-widthOfEar), tallnessOfFace];
-const topRightEarAnchor = [(availableWidth-widthOfHalfEar), availableWidth];
+const leftTopAnchor = [leftBound, tallnessOfFace];
+const leftBottomAnchor = [leftBound, padding];
 
-const rightTopAnchor = [(availableWidth-padding), tallnessOfFace];
-const rightBottomAnchor = [(availableWidth-padding), padding];
+const topLeftEarAnchor = [(leftBound+widthOfHalfEar), availableHeight];
+const leftMiddleAnchor = [(leftBound+widthOfEar), tallnessOfFace];
+
+const rightMiddleAnchor = [(widthOfFace-widthOfEar), tallnessOfFace];
+const topRightEarAnchor = [(widthOfFace-widthOfHalfEar), availableHeight];
+
+const rightTopAnchor = [(widthOfFace), tallnessOfFace];
+const rightBottomAnchor = [(widthOfFace), padding];
 
 const looseCatOutline = [
   leftTopAnchor,
@@ -38,6 +41,26 @@ const looseCatOutline = [
   ];
 
 allLines.push(looseCatOutline);
+
+
+
+
+//making the bounding box for the eyes
+const boxOffset = tallnessOfFace*(r(1/6, 1/5));
+const boxWidth = widthOfFace*(r(0.6,1));
+const boxHeight = tallnessOfFace*(r(1/4,1/2));
+const eyeBox = [
+  [((widthOfFace - boxWidth)/2), (boxHeight+boxOffset)],
+  [boxWidth, (boxHeight+boxOffset)],
+  [boxWidth, boxOffset],
+  [((widthOfFace - boxWidth)/2), boxOffset]
+  ];
+  
+allLines.push(eyeBox);
+
+
+
+
 
 drawLines(allLines);
 
