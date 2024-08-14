17f6a0251e63c9bd98e2e13952599c717e8257fe
lolasanchezz <lolansanchez@icloud.com>
2024-08-14 00:25:33 -0400

    bizarre error where editing the right eyes translation edits the left eyes translation, then when changes are reversed the error remains like there's something wrong with the editor??

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const center = (objWidth, objHeight, contWidth, contHeight) => {
-//x = (w - ww) / 2;
-//y = (h - hh) / 2;
-  const newX = ( contWidth - objWidth)/2;
-  const newY = (contHeight - contWidth)/2;
+  //x = (w - ww) / 2;
+  //y = (h - hh) / 2;
+  const newX = (contWidth - objWidth) / 2;
+  const newY = (contHeight - contWidth) / 2;
 
 };
 
@@ -23,23 +23,23 @@ setDocDimensions(width, height);
 const availableHeight = height - padding;
 const availableWidth = width - padding;
 //drawing the cat outline
-const tallnessOfFace = bt.randInRange((availableHeight*0.3), (availableHeight*0.5));
+const tallnessOfFace = bt.randInRange((availableHeight * 0.3), (availableHeight * 0.5));
 const widthOfFace = availableWidth * (r(0.6, 1));
 
-const widthOfEar = bt.randInRange(widthOfFace * 1/3, widthOfFace * 1/6);
-const widthOfHalfEar = widthOfEar*(bt.randInRange(1/4, 1/2));
-const paddingForCenter = ((availableWidth-widthOfFace)/2)
+const widthOfEar = bt.randInRange(widthOfFace * 1 / 3, widthOfFace * 1 / 6);
+const widthOfHalfEar = widthOfEar * (bt.randInRange(1 / 4, 1 / 2));
+const paddingForCenter = ((availableWidth - widthOfFace) / 2)
 
 
 
 const leftTopAnchor = [paddingForCenter, tallnessOfFace];
 const leftBottomAnchor = [paddingForCenter, padding];
 
-const topLeftEarAnchor = [(paddingForCenter+widthOfHalfEar), availableHeight];
-const leftMiddleAnchor = [(paddingForCenter+widthOfEar), tallnessOfFace];
+const topLeftEarAnchor = [(paddingForCenter + widthOfHalfEar), availableHeight];
+const leftMiddleAnchor = [(paddingForCenter + widthOfEar), tallnessOfFace];
 
-const rightMiddleAnchor = [((availableWidth-paddingForCenter)-widthOfEar), tallnessOfFace];
-const topRightEarAnchor = [((availableWidth-paddingForCenter)-widthOfHalfEar), availableHeight];
+const rightMiddleAnchor = [((availableWidth - paddingForCenter) - widthOfEar), tallnessOfFace];
+const topRightEarAnchor = [((availableWidth - paddingForCenter) - widthOfHalfEar), availableHeight];
 
 const rightTopAnchor = [(paddingForCenter + widthOfFace + padding), tallnessOfFace];
 const rightBottomAnchor = [(paddingForCenter + widthOfFace + padding), padding];
@@ -54,7 +54,7 @@ const looseCatOutline = [
   rightBottomAnchor,
   leftBottomAnchor,
   leftTopAnchor
-  ];
+];
 
 allLines.push(looseCatOutline);
 
@@ -62,51 +62,60 @@ allLines.push(looseCatOutline);
 
 
 //making the bounding box for the eyes
-const boxOffset = tallnessOfFace*(r(1/6, 1/5));
-const boxWidth = widthOfFace*(r(0.7,1));
-const boxHeight = tallnessOfFace*(r(1/3,1/1.75));
+const boxOffset = tallnessOfFace * (r(1 / 6, 1 / 5));
+const boxWidth = widthOfFace * (r(0.7, 1));
+const boxHeight = tallnessOfFace * (r(1 / 3, 1 / 1.75));
 
-const leftEyeBox = (paddingForCenter+((widthOfFace-boxWidth)/2))
-const rightEyeBoxP = leftEyeBox+boxWidth;
+const leftEyeBox = (paddingForCenter + ((widthOfFace - boxWidth) / 2))
+const rightEyeBoxP = leftEyeBox + boxWidth;
 const eyeBox = [
-  [leftEyeBox, (boxHeight+boxOffset)],
-  [leftEyeBox+boxWidth, (boxHeight+boxOffset)],
-  [leftEyeBox+boxWidth, boxOffset],
+  [leftEyeBox, (boxHeight + boxOffset)],
+  [leftEyeBox + boxWidth, (boxHeight + boxOffset)],
+  [leftEyeBox + boxWidth, boxOffset],
   [leftEyeBox, boxOffset],
-  [leftEyeBox, (boxHeight+boxOffset)],
-  ];
-  
-allLines.push(eyeBox);
+  [leftEyeBox, (boxHeight + boxOffset)],
+];
 
+allLines.push(eyeBox);
+drawLines(allLines);
 
 //making the eyes
-const eyeWidth = boxWidth*(r(0.3, 0.45));
-const eyeHeight = boxHeight*(r(0.5, 0.7));
+const eyeWidth = boxWidth * (r(0.3, 0.45));
+const eyeHeight = boxHeight * (r(0.5, 0.7));
 
-const eyePadding = eyeWidth*0.15;
-const eyePaddingY = eyeHeight*0.15;
-const outerEyeLeft = bt.catmullRom([[leftEyeBox+eyePadding,(boxOffset+eyeHeight)],
-                               [leftEyeBox+(eyeWidth), (boxOffset+eyeHeight)],
-                               [leftEyeBox+(eyeWidth), boxOffset+eyePaddingY],
-                               [leftEyeBox+eyePadding, boxOffset+eyePaddingY],
-                               [leftEyeBox+eyePadding, boxOffset+eyeHeight]], 100);
+const eyePadding = eyeWidth * 0.15;
+const eyePaddingY = eyeHeight * 0.15;
+const outerEyeLeft = bt.catmullRom([
+  [leftEyeBox + eyePadding, (boxOffset + eyeHeight)],
+  [leftEyeBox + (eyeWidth), (boxOffset + eyeHeight)],
+  [leftEyeBox + (eyeWidth), boxOffset + eyePaddingY],
+  [leftEyeBox + eyePadding, boxOffset + eyePaddingY],
+  [leftEyeBox + eyePadding, boxOffset + eyeHeight]
+], 100);
 
 
-const outerEyeRight = bt.catmullRom([[rightEyeBoxP-eyePadding, (boxOffset+eyeHeight)],
-                                    [rightEyeBoxP-eyeWidth, (boxOffset+eyeHeight)],
-                                    [rightEyeBoxP-eyeWidth, (boxOffset+eyePaddingY)],
-                                    [rightEyeBoxP-eyePadding, (boxOffset+eyePaddingY)],
-                                    [rightEyeBoxP-eyePadding, (boxOffset+eyeHeight)]],100); 
+const outerEyeRight = bt.catmullRom([
+  [rightEyeBoxP - eyePadding, (boxOffset + eyeHeight)],
+  [rightEyeBoxP - eyeWidth, (boxOffset + eyeHeight)],
+  [rightEyeBoxP - eyeWidth, (boxOffset + eyePaddingY)],
+  [rightEyeBoxP - eyePadding, (boxOffset + eyePaddingY)],
+  [rightEyeBoxP - eyePadding, (boxOffset + eyeHeight)]
+], 100);
 
 allLines.push(outerEyeLeft);
 allLines.push(outerEyeRight);
 
+//making inner eyes
+const eyeScale = 0.8;
+
+const innerEyeLeft = bt.copy(outerEyeLeft);
+bt.scale([innerEyeLeft], [eyeScale, eyeScale]);
+bt.translate([innerEyeLeft], [eyeWidth((1-eyeScale), eyeHeight*((1-eyeScale)/2)]);
+allLines.push(innerEyeLeft);
+
+
+const innerEyeRight = bt.copy(outerEyeRight);
+bt.scale([innerEyeRight], [eyeScale, eyeScale]);
+bt.translate([innerEyeLeft], [-1*(eyeWidth*((1-eyeScale)/2)), -1*(eyeHeight*((1-eyeScale)/2))]);
+
 drawLines(allLines);
-
-
-
-
-
-
-
-
