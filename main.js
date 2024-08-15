8b29e31f75ff53eb3f93db939b1074a994b6530c
lolasanchezz <lolansanchez@icloud.com>
2024-08-15 15:46:12 -0400

    second modified attempt at left stripe

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const center = (objWidth, objHeight, contWidth, contHeight) => {
   //x = (w - ww) / 2;
   //y = (h - hh) / 2;
@@ -11,7 +13,7 @@ const center = (objWidth, objHeight, contWidth, contHeight) => {
 
 const width = 125;
 const height = 125;
-const padding = 10;
+const padding = 2;
 const allLines = [];
 const r = bt.randInRange;
 setDocDimensions(width, height);
@@ -44,7 +46,7 @@ const topRightEarAnchor = [((availableWidth - paddingForCenter) - widthOfHalfEar
 const rightTopAnchor = [(paddingForCenter + widthOfFace + padding), tallnessOfFace];
 const rightBottomAnchor = [(paddingForCenter + widthOfFace + padding), padding];
 
-const looseCatOutline = bt.nurbs([
+const looseCatOutline = bt.catmullRom([
   leftTopAnchor,
   topLeftEarAnchor,
   leftMiddleAnchor,
@@ -54,7 +56,7 @@ const looseCatOutline = bt.nurbs([
   rightBottomAnchor,
   leftBottomAnchor,
   leftTopAnchor
-], {steps: 100, degree: 8});
+],100);
 //making the cat's edges jagged
 
 /*idea: keep adding random line segments together 
@@ -64,45 +66,45 @@ then replace the targeted points in the looseCatOutline array
 w/new points
 */
 
-function jaggedOutline(line, index, direction) {
-  let newPoints = [];
-  let x = 0;
-  let y = 0;
-  const jaggedness = 2;
+function jaggedOutline(line, index, direction){
+let newPoints = [];
+let x = 0;
+let y = 0;
+const jaggedness = 2;
 
-  if (direction === "vertical") {
-    let maxY = line[index + 1][1];
-    x = line[index][0];
-    y = line[index][1];
+if (direction === "vertical"){
+let maxY = line[index+1][1];
+x = line[index][0];
+y = line[index][1];
 
-    while (y >= maxY) {
-      x = x + r((-1 * jaggedness), jaggedness);
-      y = y - r(jaggedness / 2, jaggedness ^ 2);
-      newPoints.push([x, y]);
+while (y >= maxY){
+x = x + r((-1*jaggedness), jaggedness);
+y = y - r(jaggedness/2, jaggedness^2);
+newPoints.push([x,y]);
 
-    };
-  } else {
-    let maxX = line[index + 1][0];
-    x = line[index][0];
-    y = line[index][1];
+};
+} else {
+let maxX = line[index+1][0];
+x = line[index][0];
+y = line[index][1];
 
-    while (x <= maxX) {
-      x = x + r(jaggedness / 2, jaggedness ^ 2);
-      y = y + r(-1 * jaggedness, jaggedness);
-      newPoints.push([x, y]);
-
-    };
-
-  };
-
-
-  //months.splice(4, 1, 'May');
-  // Replaces 1 element at index 4
-
-  looseCatOutline.splice(index, 2, [newPoints]);
+while (x <= maxX){
+x = x + r(jaggedness/2, jaggedness^2);
+y = y + r(-1*jaggedness, jaggedness);
+newPoints.push([x,y]);
 
 };
 
+};
+
+
+//months.splice(4, 1, 'May');
+// Replaces 1 element at index 4
+  
+looseCatOutline.splice(index, 2, [newPoints]);
+  
+};
+  
 //jaggedOutline(looseCatOutline, 5, "vertical");  
 
 
@@ -119,7 +121,7 @@ months.splice(1, 0, 'Feb');
 
 //making the bounding box for the eyes
 const boxOffset = tallnessOfFace * (r(1 / 6, 1 / 5));
-const boxWidth = widthOfFace * (r(1 / 3, 1));
+const boxWidth = widthOfFace * (r(1/3, 1));
 const boxHeight = tallnessOfFace * (r(1 / 3, 1 / 1.75));
 
 const leftEyeBox = (paddingForCenter + ((widthOfFace - boxWidth) / 2))
@@ -191,40 +193,38 @@ previous for loop
 
 
 function stippleV1(shape, Density, noise, gapBetweenIterations) {
-  //parameters
-  const dotThickness = 0.25;
+//parameters
+  const dotThickness = 0.01;
   const density = Density;
-  const centerOfShape = bt.bounds([shape]).cc
-  //centerOfShape[0] = (widthOfFace/2)+padding
-  ////
+////
 
   const stiShape = bt.copy(shape);
-  while (stiShape.length > 2) {
-
-   bt.scale([stiShape], [gapBetweenIterations, gapBetweenIterations]);
-    bt.resample([stiShape], density);
-    //bt.rotate([stiShape], 25);
-    for (let i = 0; i < stiShape.length; i++) {
-      let deviationX = r(0.5, noise);
-      let deviationY = r(0.5, noise);
-      let newRightLine = [
-        [stiShape[i][0] + deviationX, stiShape[i][1] + deviationY],
-        [stiShape[i][0] + dotThickness + deviationX, stiShape[i][1] + dotThickness + deviationY]
-      ];
-      drawLines([newRightLine]);
-
-    };
-
+  while (stiShape.length > 2){
+  
+  bt.scale([stiShape], [gapBetweenIterations, gapBetweenIterations]);
+  bt.resample([stiShape], density);
+  //bt.rotate([stiShape], 25);
+  for (let i = 0; i < stiShape.length; i++){
+    let deviationX = r(0.5,noise);
+    let deviationY = r(0.5 ,noise);
+  let newRightLine = [
+    [stiShape[i][0] + deviationX, stiShape[i][1] + deviationY],
+    [stiShape[i][0] + dotThickness + deviationX, stiShape[i][1] + dotThickness + deviationY]
+  ];
+  allLines.push(newRightLine);
+  
   };
-
-
+  
+};
+  
+  
 };
 
-stippleV1(innerEyeLeft, 0.06, 2, 0.966);
-stippleV1(innerEyeRight, 0.06, 2, 0.966);
-stippleV1(outerEyeRight, 0.13, 2, 0.966);
-stippleV1(outerEyeLeft, 0.13, 2, 0.966);
-stippleV1(looseCatOutline, 0.1, 6, 0.935);
+stippleV1(innerEyeLeft, 0.17,2, 0.966);
+stippleV1(innerEyeRight, 0.17,2,0.966);
+stippleV1(outerEyeRight, 0.25,2,0.966);
+stippleV1(outerEyeLeft, 0.25,2,0.966);
+stippleV1(looseCatOutline, 0.1, 6,0.935);
 
 
 //trying to draw stripes!!
@@ -232,7 +232,7 @@ stippleV1(looseCatOutline, 0.1, 6, 0.935);
 //bottom left stripe:
 
 //const bottomMidCoords = [bt.bounds([looseCatOutline]).cb[0] - r(widthOfFace*0.1, widthOfFace*0.2),
-//bt.bounds([looseCatOutline]).cb[1]]
+                   //bt.bounds([looseCatOutline]).cb[1]]
 
 
 
@@ -244,62 +244,66 @@ const midEyeCoords = [
 ]
   */
 
-let pointMove = 1;
-//const midEyeCoords = outerEyeLeft[outerEyeLeft.indexOf(bt.bounds([outerEyeLeft]).b[0])+(pointMove)];
-//console.log(bt.pointInside(outerEyeLeft, midEyeCoords));
 
-console.log(bt.bounds([looseCatOutline]).xMin, bt.bounds([looseCatOutline]).yMin);
-console.log([looseCatOutline].indexOf([
-    bt.bounds([looseCatOutline]).xMin,
-    bt.bounds([looseCatOutline]).yMin
-    ]));
-drawLines([[
-  [bt.bounds([looseCatOutline]).cb[0], 0], 
-  [bt.bounds([looseCatOutline]).cb[0], availableHeight]
-  ]]);
-/*
-drawLines([[
-  [0, bt.bounds([looseCatOutline]).cb[1]
-*/
+const startWidth = bt.randIntInRange(5,10);
+const midWidth = bt.randIntInRange(5, 7);
+const endWidth = bt.randIntInRange(5,10);
 
-//^^equals -1????? bt.bounds is supposed to get a point from a line...
-//const midCoords = looseCatOutline[looseCatOutline.indexOf((bt.bounds([looseCatOutline])).cb) + 1];
 
-/*
+let pointMove = bt.randIntInRange(39,85);
 
-const midThickness = r(3, 7);
-const topThickness = r(-3, 3);
-const rightCurve = [midEyeCoords[0], midEyeCoords[1] - midThickness];
-const leftCurve = [midEyeCoords[0], midEyeCoords[1] - midThickness];
+const rightMidEyeCoords = outerEyeLeft[pointMove + midWidth];
+const leftMidEyeCoords = outerEyeLeft[pointMove];
+
+
+
+
+
+  
+pointMove = bt.randIntInRange(500,700);
+const leftMidCoords = looseCatOutline[pointMove + endWidth]
+const rightMidCoords = looseCatOutline[pointMove]
+
+pointMove = bt.randIntInRange(650,800);
+const leftTopCoords = looseCatOutline[pointMove]
+const leftBottomCoords = looseCatOutline[pointMove - startWidth]
 
 
 
 
 
 
-const bottomStripe = [
-  [midEyeCoords[0], midEyeCoords[1]],
-  [midEyeCoords[0], midEyeCoords[1]],
-  rightCurve,
-  [midCoords[0] + r(4, 7), midCoords[1]],
-  [midCoords[0], midCoords[1]],
-  leftCurve,
-  [midEyeCoords[0] - midThickness, midEyeCoords[1]]
-];
 
-//console.log(bottomStripe);
+
+
+
+
+const bottomStripe = [[
+  leftTopCoords,
+  rightMidEyeCoords,
+  rightMidCoords,
+  leftMidCoords,
+  leftMidEyeCoords,
+  leftBottomCoords,
+  leftTopCoords
+  ]]
+  
+
+console.log(bottomStripe);
 allLines.push(bottomStripe);
 
 //stippleV1(bottomStripe, 0.4, 4.1, 0.97);
 
 
-*/
 
-drawLines([looseCatOutline]);
+
+
 
 
 
 
 
 drawLines(allLines);
+
+
 
